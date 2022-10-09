38925def4532647b2467904998ba964cbf3e69a2
Mikhail_Myadelets <mrr.myadelets@mail.ru>
2022-10-09 15:14:49 +0300

    infr 7

--- linters/run.sh
+++ linters/run.sh
@@ -22,17 +22,17 @@ function check_log() {
 }
 
 # # ********** cppcheck ********** 
-print_header "RUN cppcheck"
-check_log "cppcheck main.cpp fib_lib/src fib_lib/include fib_lib/tests --enable=all --inconclusive --error-exitcode=1 -Ifib_lib/include --suppress=missingIncludeSystem" "\(information\)"
+# print_header "RUN cppcheck"
+# check_log "cppcheck main.cpp fib_lib/src fib_lib/include fib_lib/tests --enable=all --inconclusive --error-exitcode=1 -Ifib_lib/include --suppress=missingIncludeSystem" "\(information\)"
 
-# # ********** clang-tidy ********** 
-print_header "RUN clang-tidy"
-check_log "clang-tidy main.cpp fib_lib/src/*.cpp  fib_lib/tests/*.cpp  -warnings-as-errors=* -extra-arg=-std=c++17 -- -Ifib_lib/include -x c++" "Error (?:reading|while processing)"
-fib_lib/include/*.h
+# # # ********** clang-tidy ********** 
+# print_header "RUN clang-tidy"
+# check_log "clang-tidy main.cpp fib_lib/src/*.cpp  fib_lib/tests/*.cpp  -warnings-as-errors=* -extra-arg=-std=c++17 -- -Ifib_lib/include -x c++" "Error (?:reading|while processing)"
+# fib_lib/include/*.h
 
 
-# # ********** cpplint ********** 
-print_header "RUN cpplint"
-check_log "cpplint --extensions=cpp *.cpp fib_lib/src/*.cpp fib_lib/tests/*.cpp"    "Can't open for reading"
-check_log "cpplint --extensions=h   fib_lib/include/*.h"                        "Can't open for reading"
+# # # ********** cpplint ********** 
+# print_header "RUN cpplint"
+# check_log "cpplint --extensions=cpp *.cpp fib_lib/src/*.cpp fib_lib/tests/*.cpp"    "Can't open for reading"
+# check_log "cpplint --extensions=h   fib_lib/include/*.h"                        "Can't open for reading"
 print_header "SUCCESS"
